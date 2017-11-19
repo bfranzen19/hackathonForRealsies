70c182deb3a98696e64332d088fcf8090f26b234
wade boam <wade@heckledesign.com>
2017-11-18 20:06:23 -0700

    Fixed map center and zoom

--- public/js/main.js
+++ public/js/main.js
@@ -1,127 +1,163 @@
-
-
 // jQuery
-$(document).ready(function() {
-  console.log('js is linked')
+$(document).ready(function () {
+    console.log('js is linked')
 
-  var map = new google.maps.Map(document.getElementById('map'),{
-    center: {lat:40.0149856, lng:-105.2705456},
-    zoom: 3,
-    mapTypeId: 'terrain'
-  });
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 40.0149856,
+            lng: -105.2705456
+        },
+        zoom: 4,
+        mapTypeId: 'terrain',
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.TOP_CENTER
+        }
+    }, {
 
-  var inpPlace = document.getElementById('place')
-  var autocomplete = new google.maps.places.Autocomplete(inpPlace);
-  autocomplete.bindTo('bounds', map);
-
-
-/*
-  var marks = [];
-  var marker = new google.maps.Marker({
-    position: { lat: 40.016703, lng: -105.281401 },
-    map: map
-  });
-//marks.push(marker);
-  var marker = new google.maps.Marker({
-    position: { lat: 40.0071104, lng: -105.2581543 },
-    map: map
-  });
-  //marks.push(marker);
-  var marker = new google.maps.Marker({
-    position: { lat: 39.9999177, lng: -105.263087 },
-    map: map
-  });
-  //marks.push(marker);
-  var marker = new google.maps.Marker({
-    position: {lat:40.0149856, lng:-105.2705456},
-    map: map
-  });
-  //marks.push(marker);
-*/
+        zoomControl: true,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.LEFT_CENTER
+        },
+        scaleControl: true,
+        streetViewControl: true,
+        streetViewControlOptions: {
+            position: google.maps.ControlPosition.LEFT_TOP
+        },
+        fullscreenControl: true
+    });
 
 
 
-    // Vue
-var displayDetails = new Vue({
-           el:'#displayMe',
-           data:{
-               dataList : [],
-               panelStrings: []
-           },
-           // methods: {
-           //     addHtml: function(event){
-           //         if (event){
-           //             event.preventDefault();
-           //         }
-           //         console.log('ran');
-           //         console.log(this.dataList[0])
-           //         for(var i =0; i<this.dataList.length;i++){
-           //             this.dataList[i].panelString =
-           //                 `
-           //          <div class="card-header" role="tab" id="heading${i}">
-           //              <h5 class="mb-0">
-           //                  <a class="collapsed" data-toggle="collapse" href="#collaps${i}" aria-expanded="false" aria-controls="collapse${i}">${this.dataList[i].name} ${this.dataList[i].rating}</a>
-           //              </h5>
-           //          </div>
-           //          <div id="collaps${i}" class="collapse" role="tabpanel" aria-labelledby="heading${i}" data-parent="#accordion">
-           //              <div class="card-body">Latitude: ${this.dataList[i].location.lat}, Longitute: ${this.dataList[i].location.lng}</div>
-           //          </div>
-           //      `;
+    var inpPlace = document.getElementById('place')
+
+    var autocomplete = new google.maps.places.Autocomplete(inpPlace);
+    autocomplete.bindTo('bounds', map);
 
 
-           //         };
-           //     }
-           // }
-});
+    /*
+      var marks = [];
+      var marker = new google.maps.Marker({
+        position: { lat: 40.016703, lng: -105.281401 },
+        map: map
+      });
+    //marks.push(marker);
+      var marker = new google.maps.Marker({
+        position: { lat: 40.0071104, lng: -105.2581543 },
+        map: map
+      });
+      //marks.push(marker);
+      var marker = new google.maps.Marker({
+        position: { lat: 39.9999177, lng: -105.263087 },
+        map: map
+      });
+      //marks.push(marker);
+      var marker = new google.maps.Marker({
+        position: {lat:40.0149856, lng:-105.2705456},
+        map: map
+      });
+      //marks.push(marker);
+    */
+
+
+
+    //    // Vue
+    //    var displayDetails = new Vue({
+    //        el: '#displayMe',
+    //        data: {
+    //            dataList: [],
+    //            panelStrings: []
+    //        },
+    //        // methods: {
+    //        //     addHtml: function(event){
+    //        //         if (event){
+    //        //             event.preventDefault();
+    //        //         }
+    //        //         console.log('ran');
+    //        //         console.log(this.dataList[0])
+    //        //         for(var i =0; i<this.dataList.length;i++){
+    //        //             this.dataList[i].panelString =
+    //        //                 `
+    //        //          <div class="card-header" role="tab" id="heading${i}">
+    //        //              <h5 class="mb-0">
+    //        //                  <a class="collapsed" data-toggle="collapse" href="#collaps${i}" aria-expanded="false" aria-controls="collapse${i}">${this.dataList[i].name} ${this.dataList[i].rating}</a>
+    //        //              </h5>
+    //        //          </div>
+    //        //          <div id="collaps${i}" class="collapse" role="tabpanel" aria-labelledby="heading${i}" data-parent="#accordion">
+    //        //              <div class="card-body">Latitude: ${this.dataList[i].location.lat}, Longitute: ${this.dataList[i].location.lng}</div>
+    //        //          </div>
+    //        //      `;
+    //
+    //
+    //        //         };
+    //        //     }
+    //        // }
+    //    });
     // End Vue
 
-  $('#searchBtn').on('click', function(event) {
-    event.preventDefault();
+    $('#searchBtn').on('click', function (event) {
+        event.preventDefault();
 
-    //console.log(event);
-    var searchWhatToDo = $('#whatToDo').val();
-    var searchPlaceToGo = $('#place').val();
+        //console.log(event);
+        var searchWhatToDo = $('#whatToDo').val();
+        var searchPlaceToGo = $('#place').val();
 
-    var searchTerm =  {
-      searchWhat : searchWhatToDo,
-      searchPlace : searchPlaceToGo
-    }
+        var searchTerm = {
+            searchWhat: searchWhatToDo,
+            searchPlace: searchPlaceToGo
+        }
 
-    var resultsArr = [];
-    $.post('/search', searchTerm, (dataFromServer) => {
-      //console.log(dataFromServer, 'Server Data');
+        var resultsArr = [];
+        $.post('/search', searchTerm, (dataFromServer) => {
+            resultsArr = dataFromServer.map(function (element, index) {
 
-//console.log(dataFromServer.GooglePlace.location);
-
-resultsArr = dataFromServer.map(function(element){
-  return(new google.maps.Marker({
-    position: element.location,
-    map: map
-  }));
-
-//console.log(resultsArr, 'result maian 68');
-
-});
-
-        // displayDetails.dataList = dataFromServer;
+                if (index === 0) {
 
 
+                    map.setZoom(10);
+                    map.setCenter(element.location);
+                }
+
+
+                //console.log(element.location)
+                return (new google.maps.Marker({
+                    position: element.location,
+                    map: map
+                }));
+
+            });
+            console.log(resultsArr[0].map, 'result main 125');
+
+
+            // displayDetails.dataList = dataFromServer;
+
+
+            console.log(map, ' main 123')
 
 
 
-   //
-   // var displayDetails = new Vue({
-   //     el:'#displayMe',
-   //     data:{
-   //       dataList : dataFromServer
-   //       }
-   // });
-   //
-   //
-   //      // displayDetails.addHtml();
-   //      console.log(displayDetails.dataList);
 
 
+
+            //
+            var displayDetails = new Vue({
+                el: '#displayMe',
+                data: {
+                    dataList: dataFromServer
+                },
+                methods: {
+                    collapse: function () {
+                        console.log('Click Me')
+                    }
+                }
+            });
+            //
+            //
+            //      // displayDetails.addHtml();
+            //      console.log(displayDetails.dataList);
+
+
+        });
     });
-  });
-});  // end of jQ doc ready
+}); // end of jQ doc ready
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var googleStuff = require('./public/js/googleData');
 
 // google places API key
 // const key = 'AIzaSyCditWW_QbHS5b178NVjooCD4Fh3QocDBY'; // bt
-// const key = 'AIzaSyBgKtLj3nXJOMVpkQ_ENKb_qMa3ioxeamE';
+
 // const key = 'AIzaSyBeVcb5qQbkrqEZVBFcFK31FSnuK7qt_DM'; // bt
 const key = 'AIzaSyB_yfGwMryurHa2DyxgAlsoQWwQmm90GZA'
 
@@ -28,16 +28,22 @@ const yelpClient = yelp.client(theToken);
 
 app.use(express.static('./public'));
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 
-app.get('/', function(req,res) {
-  res.sendFile('./public/html/index.html', {root:'./'});
+app.get('/', function (req, res) {
+    res.sendFile('./public/html/index.html', {
+        root: './'
+    });
 });
 
 var searchPlaceOrig = null;
@@ -47,147 +53,99 @@ var searchWhatOrig = null;
 //var query = 'Denver, CO, United States'
 
 // google places api request
-app.post('/search', function(req,res) {
-  //console.log(req.body, 'server 46');
-searchPlaceOrig = req.body.searchPlace;
-searchWhatOrig = req.body.searchWhat;
+app.post('/search', function (req, res) {
+    searchPlaceOrig = req.body.searchPlace;
+    searchWhatOrig = req.body.searchWhat;
 
-var query2 = req.body.searchWhat + ' ' + req.body.searchPlace;
-console.log(query2,'server 54 q-2')
+    var query2 = req.body.searchWhat + ' ' + req.body.searchPlace;
+    request({
+            url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query2}&rankby=prominence&key=${key}`
+        },
+        function (error, response, reqQuery) {
+            if (error || (response.statusCode !== 200) || response.status === 'OVER_QUERY_LIMIT') {
+                console.log('error: ', error)
+                console.log('status code: ', response.statusCode)
+                res.send('oops...');
+            } else {
 
-  request({
-    url:
-    `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query2}&rankby=prominence&key=${key}`
-  },
-  function(error, response, reqQuery) {
-    if(error || (response.statusCode !== 200) || response.status === 'OVER_QUERY_LIMIT') {
-      console.log('error: ', error)
-      console.log('status code: ', response.statusCode)
-      res.send('oops...');
-    } else {
-      //console.log(reqQuery);
-      var reqQueryAsObj = JSON.parse(reqQuery);
-      //console.log(reqQueryAsObj.results);
+                var reqQueryAsObj = JSON.parse(reqQuery);
+                var placeID = reqQueryAsObj.results[0].place_id;
 
-      var placeID = reqQueryAsObj.results[0].place_id;
+                // place details request
+                request({
+                    url: `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeID}&key=${key}`
+                }, function (error, response, req) {
+                    if (error || (response.statusCode !== 200)) {
+                        console.log('error: ', error)
+                        console.log('status code: ', response.statusCode)
+                        res.send('oops...')
+                    } else {
+                        var reqAsObj = JSON.parse(req);
+                        let length = reqQueryAsObj.results.length;
+                        let top20 = [];
 
-      // place details request
-      request({
-        url: `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeID}&key=${key}`
-      }, function(error, response, req) {
-        if(error || (response.statusCode !== 200)) {
-          console.log('error: ', error)
-          console.log('status code: ', response.statusCode)
-          res.send('oops...')
-        } else {
-          var reqAsObj = JSON.parse(req);
-          let length = reqQueryAsObj.results.length;
-          let top20 = [];
+                        var workArr = Array.from(reqQueryAsObj.results);
+                        var yelpResults = [];
 
-          var workArr = Array.from(reqQueryAsObj.results);
-          var yelpResults = [];
+                        workArr.forEach(function (element) {
+                            yelpResults = getYelp(element.name);
+                            //var googleLoc = googleStuff.processGoogleData(element, yelpResults);
+                            var googleLoc = googleStuff.processGoogleData(element);
+                            top20.push(googleLoc);
 
-          workArr.forEach(function(element){
-            console.log(element.name, 'server 87');
-            yelpResults = getYelp(element.name);
-            // console.log(yelpResults, 'server 89');
-            //var googleLoc = googleStuff.processGoogleData(element, yelpResults);
-            var googleLoc = googleStuff.processGoogleData(element);
+                        });
 
-          console.log(top20, 'top20');
-            top20.push(googleLoc);
+                        // // sends the top20 array back to the main.js
+                        res.send(top20);
 
-            //console.log(top20, 'top20 server 112');
+                    }
+                });
+            }
+        });
 
+}); // end of google places api get request
 
-    });
-
-          // // sends the top20 array back to the main.js
-          res.send(top20);
-
-        }
-      });
+function getYelp(googPlace) {
+    var yelpLocations = [];
+    if (searchWhatOrig === '') {
+        searchWhatOrig = 'coffee';
     }
-  });
-
-});  // end of google places api get request
-
-function getYelp(googPlace){
-  var yelpLocations = [];
- if(searchWhatOrig === ''){
-   searchWhatOrig = 'coffee';
- }
 
 
     yelpClient.search({
-      term: searchWhatOrig,
-      location: googPlace,
-      limit : 3
-  }).then(response => {
-      var yelpDataArr = Array.from(response.jsonBody.businesses);
-      yelpDataArr.forEach(function (yelpReturnData) {
-        //googleLoc.yelpData = yelpStuff.processYelpData(yelpReturnData);
-       // console.log('server 103', gPlaceData.yelpData);
-      //gPlaceData.yelpData = yelpStuff.processYelpData(yelpReturnData);
-        //console.log(gPlaceData, 'server 100');
-        yelpLocations.push(yelpStuff.processYelpData(yelpReturnData));
-        //return(gPlaceData);
+        term: searchWhatOrig,
+        location: googPlace,
+        limit: 3
+    }).then(response => {
+        var yelpDataArr = Array.from(response.jsonBody.businesses);
+        yelpDataArr.forEach(function (yelpReturnData) {
+            //googleLoc.yelpData = yelpStuff.processYelpData(yelpReturnData);
+            // console.log('server 103', gPlaceData.yelpData);
+            //gPlaceData.yelpData = yelpStuff.processYelpData(yelpReturnData);
+            //console.log(gPlaceData, 'server 100');
+            yelpLocations.push(yelpStuff.processYelpData(yelpReturnData));
+            //return(gPlaceData);
 
-      });
-      return(yelpLocations);
-      console.log(yelpLocations, 'yelp locs');
-      console.log('Sent Something');
-      console.log('=-=-=-=-=-=-=-=-=-=-=');
+        });
+        return (yelpLocations);
+        console.log(yelpLocations, 'yelp locs');
+        console.log('Sent Something');
+        console.log('=-=-=-=-=-=-=-=-=-=-=');
 
-  }).catch(e => {
-      console.log(e);
-  });
+    }).catch(e => {
+        console.log(e);
+    });
 
 
 }
 
 
-
-// app.post('/yelpMe', function (req, res) {
-// console.log('yelpMe');
-//   client.search({
-//         term: 'Loveland Coffee',
-//         //location: 'loveland, co'
-//         location: '80538'
-//         //term: 'Four Barrel Coffee',
-//         //location: 'san francisco, ca'
-//     }).then(response => {
-
-
-//         //console.log(response.jsonBody.businesses, 'business');
-//         var temp = Array.from(response.jsonBody.businesses)
-//         // console.log(temp);
-
-//         temp.forEach(function (element) {
-//             yelpLocations.push(yelpStuff.processYelpData(element));
-
-//         })
-
-//         console.log('Sent Something')
-//         //      console.log('******Locations******', yelpLocations, '&&&&&&&&&Locations&&&&&&&&&');
-//         //console.log(response.jsonBody.businesses, 'business');
-//         //    console.log(response.jsonBody.businesses[0].name);
-//         console.log('=-=-=-=-=-=-=-=-=-=-=')
-//         res.send(yelpLocations)
-//     }).catch(e => {
-//         console.log(e);
-//     });
-
-
-// })
-
-
 /* -=-=-=-=-=-=- 404 catch -=-=-=-=-=-=- */
-app.use(function(request,response) {
-  response.send(`hmm. that's a 404, yo!`)
+app.use(function (request, response) {
+    response.send(`hmm. that's a 404, yo!`)
 })
 
 /* -=-=-=-=-=-=- app listen -=-=-=-=-=-=- */
-app.listen(8080, function() {
-  console.log('app running on port 8080')
+app.listen(8080, function () {
+    console.log('app running on port 8080')
 })
